69049a1f6feded16bd9390872dd04876e9ee8a5b
Kyle Lim <kyle.yk@outlook.com>
2021-05-06 17:19:38 +0800

    Update for parsing in Init Function

--- main.py
+++ main.py
@@ -9,21 +9,22 @@ import tkinter
 from tkinter import *
 import json
 
-from modules import auth
+from modules import auth, init
 
 
 ### Tkinter Root Settings
 root = Tk()
 root.title("Bus Ticketing System")
 ## Geometry
-HEIGHT = '510'
 WIDTH = '720'
+HEIGHT = '510'
 root.geometry(WIDTH + 'x' + HEIGHT)
 
 
 ### Calling out functions
 ## Authentication
 auth.userAuth(root)
+init.storageInit()
 
 
 
