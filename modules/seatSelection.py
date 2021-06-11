ed13a8bdb3baaf33bc146c720e2e87c0dbcd6750
Kyle Lim <kyle.yk@outlook.com>
2021-06-11 16:20:16 +0800

    Update comments

--- modules/seatSelection.py
+++ modules/seatSelection.py
@@ -1,34 +1,3 @@
-'''
-** ** ** ** **
-Code Filename: main.py
-Course: PSP0201 Mini IT Project 
-Trimester: 2030
-Lecture Section: TC1V
-Tutorial Section: TT2V
-
-Student Name as per MMU 1: Chua Hui Yi
-Student ID 1: 1201100840
-Email 1: 1201100840@student.mmu.edu.my
-Phone 1: 010-7843168
-
-Student Name as per MMU 2: Edwin Lim Cheng Yin
-Student ID 2: 1201100287
-Email 2: 1201100287@student.mmu.edu.my
-Phone 2: 016-2152148
-
-Student Name as per MMU 3: Lim Yuen Khai
-Student ID 3: 1201100842
-Email 3: 1201100842@student.mmu.edu.my
-Phone 3: 011-60977732
-
-Student Name as per MMU 4: Muhammad Haikal bin Lokman
-Student ID 4: 1201100844 
-Email 4: 1201100844@student.mmu.edu.my
-Phone 4: 019-2580817
-** ** ** ** **
-'''
-
-
 ###################
 # SEAT SELECTION #
 ##################
@@ -106,11 +75,11 @@ def exit_window(root_, total, price, seats, id, businfo):
     }
     add_json(data,'./data/ticketHistory.json')
 
-
+    
 
 # This function allows the selection of seats by highlighting them in green
 def click_button(btn):
-    def select_button():
+    def select_button(): # Nested Function 
         if btn["bg"] == "SystemButtonFace":
             btn.config(bg="green", relief=SUNKEN)
         else:
@@ -486,7 +455,6 @@ def seat_layout_30(root, busid, fare_per_seat, businfo):
                 if busid_ == (data[i].get('bus_id')):
                     index = i
                     break
-
 
             for i in range(len(button_list)):
                 # to check bg of each button:
